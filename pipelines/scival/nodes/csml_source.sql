6118d284d38dcea10efa4c6d5374c2a6b275f3b6
snorkysnark <snorkysnark@gmail.com>
2023-10-16 18:10:51 +0200

    ensure unique num_source in sources

--- pipelines/scival/nodes/csml_source.sql
+++ pipelines/scival/nodes/csml_source.sql
@@ -2,9 +2,12 @@ CREATE TABLE {{table}}(
     id_source INTEGER PRIMARY KEY AUTOINCREMENT,
     source_title TEXT,
     issn TEXT,
-    num_source TEXT,
+    num_source TEXT UNIQUE,
     source_type TEXT
 );
 
 INSERT INTO {{table}}(source_title, issn, num_source, source_type)
-SELECT DISTINCT scopus_source_title, issn, num_source, source_type FROM {{records}};
+SELECT scopus_source_title, issn, num_source, source_type
+FROM {{records}}
+GROUP BY num_source
+HAVING MIN(ROWID);

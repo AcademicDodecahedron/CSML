704ccc8a53d25f25f53fba3104f98c5e6d80587d
snorkysnark <snorkysnark@gmail.com>
2023-10-17 20:46:37 +0200

    remove redundant Path() casting

--- pipelines/scival/__main__.py
+++ pipelines/scival/__main__.py
@@ -9,7 +9,7 @@ cli = TaskRunnerCli()
 cli.add_argument("-c", "--config", type=Path, required=True, help="YAML config file")
 action, args = cli.parse_args()
 
-with Path(args.config).open() as config_file:
+with args.config.open() as config_file:
     config = ScivalConfig.model_validate(yaml.safe_load(config_file)["source"])
 
 action(create_tasks(config))

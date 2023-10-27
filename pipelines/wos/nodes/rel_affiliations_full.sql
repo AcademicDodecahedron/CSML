28287521e65288a0c3850d27425f32e12dd00066
snorkysnark <snorkysnark@gmail.com>
2023-10-27 21:58:46 +0200

    dedupe rel_affiliations

--- pipelines/wos/nodes/rel_affiliations_full.sql
+++ pipelines/wos/nodes/rel_affiliations_full.sql
@@ -9,4 +9,9 @@ SELECT id_record_affiliation, id_record_author, "order"
 FROM {{raw}} AS rel
 JOIN {{authors}} AS authors ON rel.id_record = authors.id_record AND (
     rel.author_name IS NULL OR rel.author_name = authors.full_name
-);
+)
+-- Use GROUP BY to avoid situations with authors having the same name
+-- Finger, M.; Finger, M.;
+-- [Finger, M.; Finger, M.] Charles Univ Prague, Prague, Czech Republic;
+GROUP BY id_record_affiliation, id_record_author;
+

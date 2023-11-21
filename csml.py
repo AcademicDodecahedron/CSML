38f4aa46efd0ad500326502b397219daa3e78f38
snorkysnark <snorkysnark@gmail.com>
2023-11-21 15:19:06 +0100

    use typer as cli generator

--- csml.py
+++ csml.py
@@ -1,8 +1,9 @@
+import typer
 import sqlite3
 import yaml
 from pathlib import Path
 from pydantic import BaseModel, Field
-from argparse import ArgumentParser
+from typing import Annotated
 
 from lib import TaskIndex, sql_environment, sql_adapter, console, track
 from pipelines import SourceConfig
@@ -14,26 +15,24 @@ class CsmlConfig(BaseModel):
     export: list[Path]
 
 
-if __name__ == "__main__":
-    argparser = ArgumentParser()
-    argparser.add_argument("output", type=Path, help="output sqlite database")
-    argparser.add_argument(
-        "-c", "--config", type=Path, required=True, help="YAML config file"
-    )
-    argparser.add_argument("-s", "--slice", type=int, default=0, help="id_slice value")
-    argparser.add_argument(
-        "--continue",
-        action="store_true",
-        help="don't clear the temporary database",
-        dest="continue_",  # since `continue` is a reserved keyword
-    )
-    args = argparser.parse_args()
+app = typer.Typer()
 
-    with Path(args.config).open() as config_file:
-        config = CsmlConfig.model_validate(yaml.safe_load(config_file))
 
-    temp_db: Path = args.output.with_stem(args.output.stem + ".tmp")
-    if not args.continue_:
+@app.command()
+def run(
+    output: Annotated[Path, typer.Argument(help="output sqlite database")],
+    config_file: Annotated[
+        typer.FileText, typer.Option("--config", "-c", help="YAML config file")
+    ],
+    slice: Annotated[int, typer.Option("--slice", "-s", help="id_slice value")] = 0,
+    continue_: Annotated[
+        bool, typer.Option("--continue", help="don't clear the temporary database")
+    ] = False,
+):
+    config = CsmlConfig.model_validate(yaml.safe_load(config_file))
+
+    temp_db = output.with_stem(output.stem + ".tmp")
+    if not continue_:
         temp_db.unlink(True)
 
     with sqlite3.connect(str(temp_db)) as conn:
@@ -41,8 +40,8 @@ if __name__ == "__main__":
         conn.execute("BEGIN")
         TaskIndex(config.source.create_tasks()).full_sequence().run(conn)
 
-    args.output.unlink(True)
-    with sqlite3.connect(str(args.output)) as conn:
+    output.unlink(True)
+    with sqlite3.connect(str(output)) as conn:
         conn.execute("PRAGMA foreign_keys = 1")
 
         for script_path in track(config.sql_schema, description="Preparing schema..."):
@@ -53,5 +52,9 @@ if __name__ == "__main__":
         for script_path in track(config.export, description="Exporting data..."):
             console.print(f"Executing [bold cyan]{script_path}")
             conn.executescript(
-                sql_environment.render(script_path.read_text(), slice=args.slice)
+                sql_environment.render(script_path.read_text(), slice=slice)
             )
+
+
+if __name__ == "__main__":
+    app()
